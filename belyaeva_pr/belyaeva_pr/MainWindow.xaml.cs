20b20e7911bb5d8b3eebee0455cc31cffd31a510
belyaevaAnn <67000841+belyaevaAnn@users.noreply.github.com>
2020-06-27 10:54:21 +0500

    Add files via upload

--- belyaeva_pr/belyaeva_pr/MainWindow.xaml.cs
+++ belyaeva_pr/belyaeva_pr/MainWindow.xaml.cs
@@ -15,6 +15,8 @@ using System.Windows.Shapes;
 using belyaeva_pr.sqlConn;
 using MySql.Data.MySqlClient;
 using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace belyaeva_pr
 {
@@ -26,28 +28,36 @@ namespace belyaeva_pr
         MySqlConnection conn = DBUtils.GetDBConnection();
 
         MySqlCommand com = new MySqlCommand();
-        int user_id;
+        public static int user_id;
+        public static bool privileg;
         string sql;
 
         public MainWindow()
         {
             InitializeComponent();
-            com.Connection = conn;
-            com.Parameters.Add("@analyse", MySqlDbType.Double);
-            com.Parameters.Add("@setup", MySqlDbType.Double);
-            com.Parameters.Add("@service", MySqlDbType.Double);
-            com.Parameters.Add("@time", MySqlDbType.Double);
-            com.Parameters.Add("@difficult", MySqlDbType.Double);
-            com.Parameters.Add("@money", MySqlDbType.Double);
+            try
+            {
+                conn.Open();
+                com.Connection = conn;
+                com.Parameters.Add("@analyse", MySqlDbType.Double);
+                com.Parameters.Add("@setup", MySqlDbType.Double);
+                com.Parameters.Add("@service", MySqlDbType.Double);
+                com.Parameters.Add("@time", MySqlDbType.Double);
+                com.Parameters.Add("@difficult", MySqlDbType.Double);
+                com.Parameters.Add("@money", MySqlDbType.Double);
+                com.Parameters.Add("@id", MySqlDbType.Int32);
+            }
+            catch
+            {
+                MessageBox.Show("Невозможно подключиться к базе");
+            }
         }
 
         private void enterS(object sender, RoutedEventArgs e)
         {
             try
             {
-                conn.Open();
                 sql = $"select * from user where login = '{login_enter.Text}' and password = '{password_enter.Password.ToString()}'";
-                com.Connection = conn;
                 com.CommandText = sql;
                 using (DbDataReader reader = com.ExecuteReader())
                 {
@@ -58,35 +68,66 @@ namespace belyaeva_pr
                         while (reader.Read())
                         {
                             user_id = reader.GetInt32(0);
+                            privileg = reader.GetBoolean(4);
                         }
                     }
                     else
                     {
-                        MessageBox.Show("Неверные логин или пароль");
+                        MessageBox.Show("Логин или пароль неверны");
                     }
                 }
 
-                sql = $"select * from coefficient where Id = {user_id}";
-                com.CommandText = sql;
-                using (DbDataReader reader = com.ExecuteReader())
+                if (privileg == true)
                 {
-                    if (reader.HasRows)
+                    add_task.IsEnabled = true;
+                    coef_button.IsEnabled = true;
+                    userFilter.IsEnabled = true;
+                    userFilter.Items.Add("Без фильтра");
+                    userFilter.Text = "Без фильтра";
+                    sql = $"select name from user where Id in (select idPerf from subordinates where idMen = {user_id})";
+                    com.CommandText = sql;
+                    using (DbDataReader reader = com.ExecuteReader())
                     {
-                        while (reader.Read())
+                        if (reader.HasRows)
                         {
-                            analyse_C.Text = reader[1].ToString();
-                            setup_C.Text = reader[2].ToString();
-                            service_C.Text = reader[3].ToString();
-                            time_C.Text = reader[4].ToString();
-                            difficult_C.Text = reader[5].ToString();
-                            money_C.Text = reader[6].ToString();
+                            while (reader.Read())
+                            {
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    userFilter.Items.Add(reader.GetString(i));
+                                }
+                            }
                         }
                     }
+                    sql = $"select * from coefficient where Id = {user_id}";
+                    com.CommandText = sql;
+                    using (DbDataReader reader = com.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                analyse_C.Text = reader[1].ToString();
+                                setup_C.Text = reader[2].ToString();
+                                service_C.Text = reader[3].ToString();
+                                time_C.Text = reader[4].ToString();
+                                difficult_C.Text = reader[5].ToString();
+                                money_C.Text = reader[6].ToString();
+                            }
+                        }
+                    }
+                    update_table($"SELECT task.id, user.name, title, complexity, status, natureWork, timeTask FROM task, user WHERE userID in (SELECT idPerf from subordinates where idMen = {user_id}) and task.userID = user.Id");
+                }
+                else
+                {
+                    add_task.IsEnabled = false;
+                    coef_button.IsEnabled = false;
+                    userFilter.IsEnabled = false;
+                    update_table($"SELECT task.id, user.name, title, complexity, status, natureWork, timeTask FROM task, user WHERE userID = {user_id} and task.userID = user.Id");
                 }
             }
             catch (Exception ex)
             {
-                // MessageBox.Show("Подключение к базе отсутствует");
                 MessageBox.Show(ex.Message);
             }
         }
@@ -117,5 +158,105 @@ namespace belyaeva_pr
                 MessageBox.Show("Ошибка обновления данных");
             }
         }
+
+        private void coef_button_Click(object sender, RoutedEventArgs e)
+        {
+            meneger.Visibility = Visibility.Hidden;
+            coefic.Visibility = Visibility.Visible;
+        }
+        private void back_button_Click(object sender, RoutedEventArgs e)
+        {
+            coefic.Visibility = Visibility.Hidden;
+            meneger.Visibility = Visibility.Visible;
+        }
+
+        public void update_table (string sql)
+        {
+            task_table.ItemsSource = null;
+            MySqlCommand com = new MySqlCommand();
+            com.Connection = conn;
+            com.CommandText = sql;
+            using (DbDataReader readDB = com.ExecuteReader())
+            {
+                List<task_class> task_list = new List<task_class>();
+                if (readDB.HasRows)
+                {
+                    while (readDB.Read())
+                    {
+                        for(int i=0; i<readDB.FieldCount; i+=7)
+                        {
+                            task_list.Add(new task_class() { id = readDB.GetInt32(i), id_user = readDB.GetString(i+1), name = readDB.GetString(i + 2), difficulty = readDB.GetInt32(i + 3), status = readDB.GetString(i + 4), natWork = readDB.GetString(i + 5), timeWork = readDB.GetInt32(i + 6) });
+                        }
+                    }
+                    task_table.ItemsSource = task_list;
+                }
+            }
+        }
+
+        private void add_task_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 addWind = new Window1();
+            addWind.Show();
+        }
+
+        private void update_task_Click(object sender, RoutedEventArgs e)
+        {
+            task_class path = task_table.SelectedItem as task_class;
+            UpdateTaskWin updateWin = new UpdateTaskWin(path.id, path.id_user, path.name, path.difficulty, path.status, path.natWork, path.timeWork); 
+            updateWin.Show();
+        }
+
+        private void delete_task_Click(object sender, RoutedEventArgs e)
+        {
+            task_class path = task_table.SelectedItem as task_class;
+            com.Parameters["@id"].Value = path.id;
+            sql = $"delete from task where Id = @id";
+            com.CommandText = sql;
+            int doThing = com.ExecuteNonQuery();
+            MessageBox.Show("Данные успешно удалены");
+        }
+
+        private void task_table_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            task_class tc = task_table.SelectedItem as task_class;
+            if (tc != null && (tc.status == "Завершено" || tc.status == "Отменено"))
+            {
+                update_task.IsEnabled = false;
+            }
+            else
+            {
+                update_task.IsEnabled = true;
+            }
+        }
+
+        private void update_table_button_Click(object sender, RoutedEventArgs e)
+        {
+            if (privileg)
+            {
+                if (userFilter.Text == "Без фильтра")
+                {
+                    update_table($"SELECT task.id, user.name, title, complexity, status, natureWork, timeTask FROM task, user WHERE userID in (SELECT idPerf from subordinates where idMen = {user_id}) and task.userID = user.Id");
+                }
+                else
+                {
+                    update_table($"SELECT task.id, user.name, title, complexity, status, natureWork, timeTask FROM task, user WHERE userID in (SELECT idPerf from subordinates where idMen = {user_id}) and task.userID = user.Id and user.name = '{userFilter.Text}'");
+                }
+            }
+            else
+            {
+                update_table($"SELECT task.id, user.name, title, complexity, status, natureWork, timeTask FROM task, user WHERE userID = {user_id} and task.userID = user.Id");
+            }
+        }
+    }
+
+    public class task_class
+    {
+        public int id { get; set; }
+        public string id_user { get; set; }
+        public string name { get; set; }
+        public int difficulty { get; set; }
+        public string status { get; set; }
+        public string natWork { get; set; }
+        public int timeWork { get; set; }
     }
 }
